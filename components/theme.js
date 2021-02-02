3e526c1ea35f25fd1419df0354ca4d03f41fdb93
iray100200 <iray100200@hotmail.com>
2021-02-02 18:44:05 +0800

    update

--- components/theme.js
+++ components/theme.js
@@ -1,8 +1,14 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 import { red } from '@material-ui/core/colors'
 
-// Create a theme instance.
 const theme = createMuiTheme({
+  overrides: {
+    MuiButton: {
+      root: {
+        minWidth: 52
+      }
+    }
+  },
   palette: {
     type: 'dark',
     primary: {
