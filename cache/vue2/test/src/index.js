2442af20568ba461801526df3a1e6bb4858864c3
iray100200 <iray100200@hotmail.com>
2021-02-25 11:32:57 +0800

    update

--- cache/vue2/test/src/index.js
+++ cache/vue2/test/src/index.js
@@ -1,15 +1,8 @@
-import "./styles.css"
-import React from 'react'
+import Vue from 'vue'
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <h3 onChange={this.handleChange} ok={handleOk}>I am ok</h3>
-      <h3></h3>
-      <h4 onClick={handleClick} />
-      <Name />
-    </div>
-  )
-}
+new Vue({
+  el: '#app',
+  render () {
+    return <div>Vue sample</div>
+  }
+})
